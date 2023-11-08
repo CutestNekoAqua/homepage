48a3f74cfe50416022e2cbddb39766f263f19890
April John <waterdev@galaxycrow.de>
2023-11-08 01:15:58 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,71 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+	import logo from '$lib/images/svelte-logo.svg';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Made with 💜 and 
+			<a href="https://kit.svelte.dev">
+				<img src={logo} alt="SvelteKit" />
+			</a>
+		</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+		color: white;
+	}
+	
+
+	footer p {
+		display: flex;
+		align-items: center;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+
+	footer img {
+		width: 2em;
+		height: 2em;
+		object-fit: scale-down;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>April's Homepage</title>
+	<meta name="description" content="Aprils Homepage" />
+</svelte:head>
+
+<section>
+	<span class="main" />
+
+	<div class="card">
+		<div id="console" />
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	.card {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+		background: lightgray;
+		padding: 15px 25px 15px 25px;
+		box-shadow: 7px 7px rgba(152, 232, 252, 0.5);
+		border: 2px solid #4cb6eb;
+	}
+
+	#console {
+		font-family: courier, monospace;
+		color: #000;
+		width: 750px;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 30px;
+		font-size: 14px;
+	}
+
+	a {
+		color: #0bc;
+		text-decoration: none;
+	}
+
+	#a {
+		color: #0f0;
+	}
+
+	#c {
+		color: #0bc;
+	}
+
+	#b {
+		color: #ff0096;
+	}
+
+	#k {
+		animation: change 1s;
+	}
+
+	#op {
+		color: #888888;
+	}
+
+	@keyframes change {
+		0% {
+			color: #0f0;
+		}
+		50% {
+			color: #0f0;
+		}
+		99% {
+			color: black;
+		}
+	}
+</style>
